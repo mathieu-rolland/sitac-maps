c9b559ec7649dfacf71ea717d5577ea06dc18b1d
christophe coelo <c.coelo@gmail.com>
2014-02-19 10:08:44 +0100

    [#3.2] Ajout test maps

---
+++ src/com/istic/sit/sitac_maps/MainActivity.java
@@ -0,0 +1,14 @@
+package com.istic.sit.sitac_maps;
+
+import android.os.Bundle;
+
+import com.istic.sit.framework.model.MapsActivity;
+
+public class MainActivity extends MapsActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);		
+	}
+
+}
