acb6983ff30a405c386715a89002bfa39d470a8f
christophe coelo <c.coelo@gmail.com>
2014-03-19 08:37:13 +0100

    Ajout test ajout initial

--- src/com/istic/sit/sitac_maps/MainActivity.java
+++ src/com/istic/sit/sitac_maps/MainActivity.java
@@ -2,13 +2,18 @@ package com.istic.sit.sitac_maps;
 
 import android.os.Bundle;
 
-import com.istic.sit.framework.model.MapsActivity;
+import com.istic.sit.framework.api.model.IEntity;
+import com.istic.sit.framework.model.Entity;
+import com.istic.sit.framework.view.MapsActivity;
 
 public class MainActivity extends MapsActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);		
+		super.onCreate(savedInstanceState);	
+		
+		
+		testAddMarker();
 	}
 
 }
