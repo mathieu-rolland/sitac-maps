89d985cd6295f145b608aca168efe194060f6c60
vincent <vincent.bodiou@gmail.com>
2014-03-24 16:05:00 +0100

    rendre le menu parametrable

--- src/com/istic/sit/sitac_maps/MapsActivity.java
+++ src/com/istic/sit/sitac_maps/MapsActivity.java
@@ -1,7 +1,9 @@
 package com.istic.sit.sitac_maps;
 
 import android.os.Bundle;
+import android.view.Menu;
 
+import com.istic.sit.R;
 import com.istic.sit.framework.view.MainActivity;
 
 public class MapsActivity extends MainActivity {
@@ -10,9 +12,21 @@ public class MapsActivity extends MainActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		initializeBackground(TypeBackgroundEnum.Map);
+		initializeBackground(TypeBackgroundEnum.Map, savedInstanceState);
 	}
 	
+	@Override
+	public void onItemMenuClicked(int position) {
+		
+	}
+	
+	@Override
+	public void onCreateSlideMenu()
+	{
+		this.addItemMenu(getResources().getString(R.string.menu_dynamic));
+		this.addItemMenu(getResources().getString(R.string.menu_virtuel));
+		this.addItemMenu(getResources().getString(R.string.menu_static));
+	}
 }
 
 
