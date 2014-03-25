8131adf9927b64dfcb64d169e88f381eb512fa1a
vincent <vincent.bodiou@gmail.com>
2014-03-25 15:53:39 +0100

    dnd

--- src/com/istic/sit/sitac_maps/MapsActivity.java
+++ src/com/istic/sit/sitac_maps/MapsActivity.java
@@ -1,10 +1,19 @@
 package com.istic.sit.sitac_maps;
 
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.DragShadowBuilder;
+import android.widget.TextView;
 
 import com.istic.sit.R;
+import com.istic.sit.framework.api.model.IEntity;
+import com.istic.sit.framework.api.view.IBackground;
+import com.istic.sit.framework.model.DynamicEntity;
+import com.istic.sit.framework.model.Representation;
 import com.istic.sit.framework.view.MainActivity;
+import com.istic.sit.framework.view.MapFragment;
 
 public class MapsActivity extends MainActivity {
 
@@ -16,7 +25,7 @@ public class MapsActivity extends MainActivity {
 	}
 	
 	@Override
-	public void onItemMenuClicked(int position) {
+	public void onItemMenuClicked(int position, View view) {
 		
 	}
 	
@@ -26,6 +35,31 @@ public class MapsActivity extends MainActivity {
 		this.addItemMenu(getResources().getString(R.string.menu_dynamic));
 		this.addItemMenu(getResources().getString(R.string.menu_virtuel));
 		this.addItemMenu(getResources().getString(R.string.menu_static));
+	}
+
+	@Override
+	public void onItemMenuLongClicked(int position, View view) {
+		String entityString = (String) this.entityAdapter.getItem(position);
+		DragShadowBuilder entityShadow = new DragShadowBuilder(view);
+
+		view.startDrag(null,	//ClipData
+				entityShadow,   //View.DragShadowBuilder 
+				entityString,	//Object myLocalState
+                0);	
+		
+		this.mDrawerLayout.closeDrawer(listMenu);				
+	}
+
+	@Override
+	public boolean onActionDropFromMenu() {		
+		
+		IEntity entity = new DynamicEntity();
+
+		entity.setRepresentation(new Representation(R.drawable.logoagetac72));
+		entity.setLibelle("test");
+		
+		((IBackground) getFragment()).addEntity(entity);
+		return true;
 	}
 }
 
