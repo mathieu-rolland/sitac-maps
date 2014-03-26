e3fc63817859b79ddc7fca5fccd069845074bbb7
vincent <vincent.bodiou@gmail.com>
2014-03-26 15:36:36 +0100

    menu d'entité

--- src/com/istic/sit/sitac_maps/MapsActivity.java
+++ src/com/istic/sit/sitac_maps/MapsActivity.java
@@ -1,19 +1,13 @@
 package com.istic.sit.sitac_maps;
 
 import android.os.Bundle;
-import android.view.DragEvent;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.DragShadowBuilder;
-import android.widget.TextView;
-
 import com.istic.sit.R;
 import com.istic.sit.framework.api.model.IEntity;
-import com.istic.sit.framework.api.view.IBackground;
 import com.istic.sit.framework.model.DynamicEntity;
 import com.istic.sit.framework.model.Representation;
 import com.istic.sit.framework.view.MainActivity;
-import com.istic.sit.framework.view.MapFragment;
 
 public class MapsActivity extends MainActivity {
 
@@ -38,25 +32,36 @@ public class MapsActivity extends MainActivity {
 	
 	@Override
 	public void onCreateSlideMenu()
-	{
-		this.addItemMenu(getResources().getString(R.string.menu_dynamic));
-		this.addItemMenu(getResources().getString(R.string.menu_virtuel));
-		this.addItemMenu(getResources().getString(R.string.menu_static));
+	{		
+		IEntity entityDynamic = new DynamicEntity();
+		entityDynamic.setLibelle(getResources().getString(R.string.menu_dynamic));
+		entityDynamic.setRepresentation(new Representation(R.drawable.ic_camion));
+		
+		IEntity entityVirtuel = new DynamicEntity();
+		entityVirtuel.setLibelle(getResources().getString(R.string.menu_virtuel));
+		entityVirtuel.setRepresentation(new Representation(R.drawable.ic_bouche_incendie));
+		
+		IEntity entityStatic = new DynamicEntity();
+		entityStatic.setLibelle(getResources().getString(R.string.menu_static));
+		
+		this.addItemMenu(entityDynamic);
+		this.addItemMenu(entityVirtuel);
+		this.addItemMenu(entityStatic);
 	}
 
 	@Override
 	public void onItemMenuLongClicked(int position, View view) {
-		String entityString = (String) this.entityAdapter.getItem(position);
+		IEntity entity = (IEntity)this.entityAdapter.getItem(position);
+		
 		DragShadowBuilder entityShadow = new DragShadowBuilder(view);
 
 		view.startDrag(null,	//ClipData
 				entityShadow,   //View.DragShadowBuilder 
-				entityString,	//Object myLocalState
-                0);	
+				entity,	//Object myLocalState
+                0);
 		
 		this.mDrawerLayout.closeDrawer(listMenu);				
 	}
-
 }
 
 
